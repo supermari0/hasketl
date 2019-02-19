76bc25f8af56c19ac8d1c58cc19df38740edada0
Mario Villaplana <mario.villaplana@rackspace.com>
2019-02-19 12:18:03 -0600

    Add another import and draft of program logic

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,6 @@ module Main where
 
 import Lib
 
+-- main is a stub that imports Lib
 main :: IO ()
 main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE DuplicateRecordFields #-} -- Allow duplicate record fields
 module Lib
     ( someFunc
     ) where
+import qualified Database.HDBC as HDBC
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Row = Row [ HDBC.SqlValue ]
+
+{-
+ - General program flow:
+ - - (Possibly) Execute SQL against MSSQL DB from types we define
+ - - Reason: We can run limited versions of these queries to test whether they
+ - will work.
+ - - Note: The above may involve some more complex logic, like joins, filtering
+ - out account banlists, etc.
+ - - Run series of postprocessing steps in BigQuery to generate further tables
+ - - Run tests against those tables validating they meet our assumptions (table
+ - unchanged, only rows added, etc.)
+ -}
